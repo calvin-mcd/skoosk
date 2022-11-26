a9f501feb4a7ab47c5dd5aad60f54e53361a8676
calvin-mcd <114294748+calvin-mcd@users.noreply.github.com>
2022-11-26 16:22:24 +0100

    repairing schematics, changing pcb

---
+++ Firmware/Source Code/config.h
@@ -0,0 +1,24 @@
+#pragma once
+
+#define ENCODERS_PAD_A { GP9 }
+#define ENCODERS_PAD_B { GP21 }
+
+#define MIDI_ADVANCED
+
+/* RGB Pin is GP1. */
+
+/*
+ * Feature disable options
+ *  These options are also useful to firmware size reduction.
+ */
+
+/* disable debug print */
+//#define NO_DEBUG
+
+/* disable print */
+//#define NO_PRINT
+
+/* disable action features */
+//#define NO_ACTION_LAYER
+//#define NO_ACTION_TAPPING
+//#define NO_ACTION_ONESHOT
---
+++ Firmware/Source Code/keymaps/vial/keymap.c
@@ -0,0 +1,19 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT(
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,   KC_NO,
+        KC_NO,   KC_NO,   KC_NO,   KC_NO,            KC_NO,
+        KC_NO,            KC_NO,            KC_NO,   KC_NO
+    ),		
+};
+
+/* Encoder */ 
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] = { ENCODER_CCW_CW(KC_TRNS, KC_TRNS) }
+};
